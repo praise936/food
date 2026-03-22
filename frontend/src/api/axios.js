1de0fda59f40dada1f342416974eb9dfdf686844
praise936 <peterpraise259@gmail.com>
2026-03-22 18:46:01 +0300

    fix six

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 // Base URL for all API requests
 const api = axios.create({
-    baseURL: 'https://semifitted-neymar-granular.ngrok-free.dev/api',
+    baseURL: 'https://foodcourt-production.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json',
     },
