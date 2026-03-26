069ae489b85761b751ed340e9522bd2d543d3034
praise936 <peterpraise259@gmail.com>
2026-03-26 12:37:19 +0300

    update 34

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -35,11 +35,13 @@ const CartPage = () => {
                     quantity: item.quantity,
                 })),
             }
+            console.log("trying to see if we can clear cat")
 
             await api.post('/orders/place/', orderData)
-
+            console.log("Order success, clearing cart")
             // Clear cart immediately after successful order — before navigation
             clearCart()
+            console.log("Order success, cat cleared")
             setNotes('')
 
             toast.success('🎉 Order placed successfully!')
@@ -196,10 +198,7 @@ const CartPage = () => {
 
                                 {/* Place order button */}
                                 <button
-                                    onClick={()=>{
-                                        handlePlaceOrder()
-                                        clearCart()
-                                    }}
+                                    onClick={handlePlaceOrder}
                                     disabled={placing}
                                     className="btn-primary w-full flex items-center justify-center gap-2 py-3">
                                     {placing ? (
