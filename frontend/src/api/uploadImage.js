691e53e16005eecbedd70e83aca80e8054955fff
praise936 <peterpraise259@gmail.com>
2026-03-28 10:26:28 +0300

    update 37

--- frontend/src/api/uploadImage.js
+++ frontend/src/api/uploadImage.js
@@ -7,7 +7,7 @@ export const uploadImage = async (file) => {
     const fileName = `${Date.now()}-${file.name}`
 
     const { error } = await supabase.storage
-        .from("mood-images")
+        .from("images")
         .upload(fileName, file)
 
     if (error) {
